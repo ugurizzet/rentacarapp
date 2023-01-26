d6ae24825b59b7f64141bde17f42d63c1ff4f6c4
izzet Onal <izzetonal46@gmail.com>
2023-01-26 19:51:40 +0100

    reservation model add

--- car/models.py
+++ car/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator
+from django.contrib.auth.models import User
 class Car(models.Model):
     GEAR = (
         ('a', 'automatic'),
@@ -11,10 +12,30 @@ class Car(models.Model):
     model = models.CharField(max_length=20)
     year = models.SmallIntegerField()
     gear = models.CharField(max_length=1, choices=GEAR)
-    rent_per_day = models.SmallIntegerField()
+    # rent_per_day = models.SmallIntegerField()
+    rent_per_day = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+        validators=[MinValueValidator(100)]
+    )
     availability = models.BooleanField(default=True)
     
     def __str__(self):
         return f'{self.brand} - {self.model} - {self.plate_number}'
 
 
+class Reservation(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='customers' )
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='cars' )
+    start_date = models.DateField()
+    end_date = models.DateField()
+    
+    def __str__(self):
+        return f'Customer {self.customer} reserved {self.car} on {self.start_date} and {self.end_date}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['customer','start_date','end_date'], name='user_rant_date'
+            )
+        ]
